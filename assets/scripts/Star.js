3bcedee4063c2de2e670e7981c16c6e785a3b416
SuZhao <suzhao10086@gmail.com>
2020-01-27 19:13:46 +0800

    Add Star control

--- assets/scripts/Star.js
+++ assets/scripts/Star.js
@@ -31,9 +31,32 @@ cc.Class({
         pickRadius: 0
     },
 
+    getPlayerDistance: function () {
+        // 根据 player 节点位置判断距离
+        var playerPos = this.game.player.getPosition();
+        // 根据两点位置计算两点之间距离
+        var dist = this.node.position.sub(playerPos).mag();
+        return dist;
+    },
+
+    onPicked: function() {
+        // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+        this.game.spawnNewStar();
+        // 然后销毁当前星星节点
+        this.node.destroy();
+    },
+
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
+    update: function (dt) {
+        // 每帧判断和主角之间的距离是否小于收集距离
+        if (this.getPlayerDistance() < this.pickRadius) {
+            // 调用收集行为
+            this.onPicked();
+            return;
+        }
+    },
 
     start () {
 
